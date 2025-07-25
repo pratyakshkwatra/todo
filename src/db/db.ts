3e85dabbdf9ca689a14f7b7177be67cba1f37b2e
pratyakshkwatra <pratyakshkwatra@gmail.com>
2025-07-26 00:03:46 +0530

    setup: drizzle (pg), chore: add todo schema, drizzle config

---
+++ src/db/db.ts
@@ -0,0 +1,7 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in .env');
+}
+
+const db = drizzle(process.env.DATABASE_URL);
---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, varchar, date } from "drizzle-orm/pg-core";
+
+export const todosTable = pgTable("todos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  createdAt: date().notNull(),
+});
